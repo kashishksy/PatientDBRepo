425e1a53e1c2c4ee72a6083f6c766e56ba6c805e
kashishksy <kashishksy2112@gmail.com>
2024-11-13 16:23:09 -0500

    modifying patient model to accomodate medical reports

--- model/patientModel.js
+++ model/patientModel.js
@@ -33,7 +33,22 @@ const patientSchema = new mongoose.Schema({
     photo: {
         type: String,  // Stores URL or Base64 string of the photo
         required: false  // Optional field
-    }
+    },
+    medicalReports: [{
+        type: {
+            type: String, // Type of report (e.g., "ECG", "Kidney Function", "Blood Test")
+            required: true
+        },
+        date: {
+            type: String, // Date the report was created or added
+            required: true
+        },
+        result: {
+            type: String, // Detailed result or description of the report
+            required: false
+        },
+        attachments: [String] // Array of URLs or file paths to report attachments if any
+    }]
 
 })
 
